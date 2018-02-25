f595be74036c178d1638ef432f9a2ba347829838
yeti2018 <yeti@yetis-MacBook-Pro.local>
2018-02-25 15:56:22 -0800

    setup routes and controllers

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var wiki = require('./routes/wiki');
+var catalog = require('./routes/catalog');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/wiki', wiki);
+app.use('/catalog', catalog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
